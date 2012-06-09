5c7a55b718fb46c6ef6511de127e28705e68e0ab
NoahY <noahy57@gmail.com>
2012-06-09 19:40:28 +0530

    lang permission problem

--- qa-cv-overrides.php
+++ qa-cv-overrides.php
@@ -5,7 +5,19 @@
 		$permits[] = 'permit_vote_c';
 		return $permits;
 	}
-						
+	function qa_get_request_content() {
+		$qa_content = qa_get_request_content_base();
+		
+		// permissions
+		
+		if(isset($qa_content['form_profile']['fields']['permits'])) {			
+			
+				$ov = $qa_content['form_profile']['fields']['permits']['value'];
+				$ov = str_replace('[profile/permit_vote_c]',qa_lang('comment_voting/permit_vote_c'),$ov);
+				$qa_content['form_profile']['fields']['permits']['value'] = $ov;
+		}
+		return $qa_content;
+	}						
 /*							  
 		Omit PHP closing tag to help avoid accidental output
 */							  
